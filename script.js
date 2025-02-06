b20d25a7235a8df9fc201dda9edf670fb1b88ce3
TomvanD298 <tom@realafford.nl>
2025-02-06 16:30:36 +0100

    loader

--- script.js
+++ script.js
@@ -8,6 +8,23 @@ const engine = new liquidjs.Liquid();
 engine.parseAndRender(template.innerHTML, {persons: derulo.data}).then(html => result.innerHTML = html);
 
 
+const rocket = document.querySelector('.rocket');
+const loader = document.querySelector('.loader');
+
+// Start de eerste animatie op .rocket
+rocket.style.animation = "slide-top 1s ease-in-out alternate 6 forwards";
+
+// Wacht tot de heen-en-weer animatie op .rocket klaar is en start dan de tweede animatie
+rocket.addEventListener('animationend', () => {
+    // Tweede animatie op .rocket
+    rocket.style.animation = "slide-out-blurred-right 0.45s cubic-bezier(0.755, 0.050, 0.855, 0.060) forwards";
+    
+    // Start de animatie op .loader na een korte vertraging
+    setTimeout(() => {
+        loader.style.animation = "slide-out-blurred-right 0.45s cubic-bezier(0.755, 0.050, 0.855, 0.060) forwards";
+    }, 450); // Vertraging van 450ms, gelijk aan de duur van de tweede animatie op .rocket
+});
+
 const numberOfStars = 1000;
 
 for (let i = 0; i < numberOfStars; i++) {
@@ -24,3 +41,4 @@ for (let i = 0; i < numberOfStars; i++) {
 
     document.body.appendChild(stars);
 }
+
