24f03d277b6b7794341e7d54f094b2af5c87b780
TomvanD298 <tom@realafford.nl>
2025-02-12 11:50:00 +0100

    final touches

--- js/api.js
+++ js/api.js
@@ -2,7 +2,7 @@ const fetched = await fetch('https://fdnd.directus.app/items/person/?filter={"_a
 const derulo = await fetched.json();
 
 const template = document.querySelector('template');
-const result = document.querySelector('#result');
+const result = document.querySelector('.result');
 const engine = new liquidjs.Liquid();
 
 engine.parseAndRender(template.innerHTML, {persons: derulo.data}).then(html => result.innerHTML = html);
